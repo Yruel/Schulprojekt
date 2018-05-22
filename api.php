fa8aae6af69c04d40e850bcaa593acd1f5fab36c
Alexander Stueben <Alexander.Stueben@md.de>
2018-05-22 14:04:00 +0200

    Produktansicht

--- api.php
+++ api.php
@@ -8,13 +8,22 @@ $conn = new PDO("mysql:dbname=".DBNAME.";host=".DBHOST, DBUSER, DBPASS);
             $sql = "select * from schulprojekt";
             $category = $_GET["category"];
 
-            $query = "SELECT * FROM schulprojekt.products WHERE category = :category";
+            $query = "SELECT ID, name, brand FROM schulprojekt.products WHERE category = :category";
             $stmt = $conn->prepare($query);
             $stmt->bindParam(":category", $category);
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
             echo json_encode($result);
-        break;
+            break;
+        case "product":
+            $id = $_GET["id"];
+            $query = "SELECT * FROM schulprojekt.products WHERE ID = :id";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            echo json_encode($result[0]);
     }
 ?>
--- classes/js/products.js
+++ classes/js/products.js
@@ -4,17 +4,31 @@ $(document).ready(function (){
         url : "api.php?action=products&category=sound",
         dataType: 'json',
         success : function(result) {
-            console.log(result[0]);
             for(var i = 0; i < result.length; i++){
                 div.innerHTML +=
-                    "<div onclick='loadProduct();' class='product'>" +
+                    "<div onclick='loadProduct("+result[i].ID+");' class='product'>" +
                         "<img src='img/products/"+result[i].ID+".png'>" +
-                        "<p>"+result[i].name+"</p>" +
+                        "<p>"+result[i].brand+" "+result[i].name+"</p>" +
                     "</div>";
             }
         }
     });
 });
-function loadProduct() {
-    console.log("Hello");
+function loadProduct(value) {
+    $("#productModal").modal("toggle");
+    $.ajax({
+        url: "api.php?action=product&id="+value,
+        dataType: 'json',
+        success: function (result) {
+            document.getElementById("productLabel").innerText = result.brand+" "+result.name;
+            document.getElementById("productImage").setAttribute("src", "img/products/"+result.ID+".png");
+            document.getElementById("productPrice").innerText = result.vk+"€";
+            $.get({
+                url: "classes/descriptions/descriptions.json",
+                success: function(data, status){
+                    document.getElementById("productDescription").innerHTML = data[result.ID];
+                }
+            });
+        }
+    });
 }
