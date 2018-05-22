fbe40edd26f46c33d17284cce5835b840da8bbb2
Lukas Kemper <Lukas.Kemper@md.de>
2018-05-22 12:09:05 +0200

    Navbar style

--- classes/function.js
+++ classes/function.js
@@ -4,15 +4,6 @@ function ShowLogin() {
 function ShowRegister() {
     $("#register-modal").modal("toggle");
 }
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
 function getTest() {
     $.ajax({
 		type : "GET",
