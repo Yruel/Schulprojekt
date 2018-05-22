c81c4c332ef64467c8cc19fbf35f4cc7bb50cb12
Alexander Stueben <Alexander.Stueben@md.de>
2018-05-22 12:13:08 +0200

    Produktseite

---
+++ classes/js/products.js
@@ -0,0 +1,20 @@
+$(document).ready(function (){
+    var div = document.getElementById("products");
+    $.ajax({
+        url : "api.php?action=products&category=sound",
+        dataType: 'json',
+        success : function(result) {
+            console.log(result[0]);
+            for(var i = 0; i < result.length; i++){
+                div.innerHTML +=
+                    "<div onclick='loadProduct();' class='product'>" +
+                        "<img src='../img/products/"+result[i].ID+".png'>" +
+                        "<p>"+result[i].name+"</p>" +
+                    "</div>";
+            }
+        }
+    });
+});
+function loadProduct() {
+    console.log("Hello");
+}
