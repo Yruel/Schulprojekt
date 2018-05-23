470d4383a99e06f1b54608e49a22d3e24f6acba6
Alexander Stueben <Alexander.Stueben@md.de>
2018-05-23 10:34:34 +0200

    weiter Produnktbeschreibungen

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@ include("classes/phplib.template.class.php");
 
 define('DBUSER','root');
 define('DBPORT','3306');
-define('DBPASS','');
+define('DBPASS','Maleen491');
 define('DBNAME', 'schulprojekt');
 define('DBHOST','127.0.0.1');
 
